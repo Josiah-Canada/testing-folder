4fb67e17b77b56fef13ee72d3e3fd83010dc6ecb
Josiah-Canada <josiahcanada@yahoo.com>
2022-08-10 13:11:11 -0400

    added crud for user back end

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,23 +1,23 @@
 const {
     getAllUsers,
-    // createUser,
-    // getUserById,
-    // updateUser,
-    // deleteUser
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser
    
    } = require('../../controllers/user-controller');
  
  const router = require('express').Router();
  
- router.route("/").get(getAllUsers)
+ router.route("/users").get(getAllUsers)
  
-//  router.route("/").post(createUser)
+ router.route("/").post(createUser)
  
-//  router.route("/:id").get(getUserById)
+ router.route("/:id").get(getUserById)
  
-//  router.route('/:id/').put(updateUser)
+ router.route('/:id/').put(updateUser)
  
-//  router.route('/:id/').delete(deleteUser);
+ router.route('/:id/').delete(deleteUser);
  
  
  module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,8 @@ const apiRoutes = require("../routes/api/user-routes")
 
 router.use("/api", apiRoutes)
 
+router.use("/api/users", apiRoutes)
+
 
 
 module.exports = router;
